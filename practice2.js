327e0fe7964682c8a8ee42c1fe3c02b86b04ef0a
Irmak Erdem <irmakerdem9@gmail.com>
2021-12-16 16:17:25 -0700

    Add file

---
+++ practice2.js
@@ -0,0 +1,36 @@
+//create an object make it your name
+var irmak = {
+
+}
+
+//add to that object your age but not inside the curly braces
+irmak.age = 30;
+console.log(irmak);
+
+
+
+//object is a more powerful array
+//array is a more powerful object
+//uses dot notation to call and reassign
+
+
+
+
+//add the property of pronouns and pass it a string
+irmak.pronoun = "she/her"
+console.log(irmak);
+
+
+
+
+
+
+//create a new variable and assign it a value of this string (uses escapes)
+var thisString = ("Eli said \"don't mess this up\"");
+console.log(thisString);
+
+var thisString = ("Eli said 'don't mess this up'");
+console.log(thisString);
+
+var thisString = ('Eli said \'don\'t mess this up\'');
+console.log(thisString);
